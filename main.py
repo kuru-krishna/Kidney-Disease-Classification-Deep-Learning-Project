5553e0c664e008a803979f2675e3d02059b9bb97
kuru-krishna <rvnthng17@gmail.com>
2025-04-14 15:05:01 -0700

    model training added

--- main.py
+++ main.py
@@ -1,28 +1,9 @@
-import tensorflow as tf
-# Enable eager execution for TensorFlow 2.x
-tf.compat.v1.enable_eager_execution()
-
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 
-def run_stage(stage_name, pipeline_class):
-    try:
-        logger.info(f"*******************")
-        logger.info(f">>>>>> stage {stage_name} started <<<<<<")
-        pipeline = pipeline_class()
-        pipeline.main()
-        logger.info(f">>>>>> stage {stage_name} completed <<<<<<\n\nx==========x")
-    except Exception as e:
-        logger.exception(e)
-        raise e
 
-
-# Running each stage with error handling and logging
-run_stage("Data Ingestion", DataIngestionTrainingPipeline)
-run_stage("Prepare Base Model", PrepareBaseModelTrainingPipeline)
-run_stage("Model Training", ModelTrainingPipeline)
 
 
 
